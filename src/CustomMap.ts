8aa1145a05cc5794382c2cbbe94c72350fd73d86
Tien Pham <tiendev94@gmail.com>
2019-06-30 19:31:10 +0300

    implicit

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,15 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -7,6 +19,16 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     });
   }
